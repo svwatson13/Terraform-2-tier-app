935f53518357325659ef97ac6d1f6cb1aaee240d
Sam Watson <SWatson@spartaglobal.com>
2020-01-29 15:34:16 +0000

    done

--- main.tf
+++ main.tf
@@ -4,52 +4,29 @@ provider "aws" {
 }
 
 # create vpc
-resource "aws_vpc" "app_vpc" {
+resource "aws_vpc" "two-tier-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = var.name
   }
 }
 
-# Create security group
-resource "aws_security_group" "app_security" {
-  name        = var.name
-  description = "Allow port 80"
-  vpc_id      = aws_vpc.app_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = var.name
-  }
-}
-
 # Internet Gateway
-resource "aws_internet_gateway" "app_IG" {
-  vpc_id = aws_vpc.app_vpc.id
+resource "aws_internet_gateway" "two-tier-IG" {
+  vpc_id = aws_vpc.two-tier-vpc.id
 
   tags = {
     Name = "${var.name} - IG"
   }
 }
 
-# Route table
-resource "aws_route_table" "app_route" {
-  vpc_id = aws_vpc.app_vpc.id
+# Route table to allow internet comms
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.two-tier-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.app_IG.id
+    gateway_id = aws_internet_gateway.two-tier-IG.id
   }
 
   tags = {
@@ -58,26 +35,30 @@ resource "aws_route_table" "app_route" {
   }
 }
 
-# Route table assocations
-resource "aws_route_table_association" "app_route_association" {
-  subnet_id = aws_subnet.app_subnet.id
-  route_table_id = aws_route_table.app_route.id
+# Route table assocations - connecting public subnet with internet route
+resource "aws_route_table_association" "public_route_association" {
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route.id
 }
 
-# Create subnet
-resource "aws_subnet" "app_subnet" {
-  vpc_id = aws_vpc.app_vpc.id
+# Create subnets
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.two-tier-vpc.id
   cidr_block = "10.0.0.0/24"
   availability_zone = "eu-west-1a"
   tags = {
-    Name = "${var.name} - app"
+    Name = "${var.name} - public"
   }
-
-resource "aws_subnet" "db_subnet" {
-  vpc_id = aws_vpc.db_vpc.id
+}
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.two-tier-vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "eu-west-1a"
   tags = {
-    Name = "${var.name} - db"
+    Name = "${var.name} - private"
   }
 }
+# Route table to allow comms between subnets
+resource "aws_route_table" "public_private_route" {
+  vpc_id = aws_vpc.two-tier-vpc.id
+  }
